fe0239ad41b5baadef9382db064f4fb0a4e29d93
D <igothippie@yahoo.com>
2025-11-15 08:13:12 -0500

    Fix collapsible button icon sources - swap Base.png and Base_click.png
    - Swapped icon sources for Special/Synth buttons so plus/minus icons display correctly
    - Swapped icon sources for Merc button
    - Plus icon (Base.png) now shows when collapsed, minus icon (Base_click.png) shows when expanded
    - Small collapsible buttons already use correct open.png/closed.png convention

--- scripts/modules/class_pages.py
+++ scripts/modules/class_pages.py
@@ -1352,8 +1352,8 @@ class ClassPagesHTMLGenerator:
         
         return f"""
         <button type="button" class="collapsible runewords-button">
-            <img src="icons/Runewords_click.png" alt="Runewords Open" class="icon open-icon">
-            <img src="icons/Runewords.png" alt="Runewords Close" class="icon close-icon hidden">
+            <img src="icons/Runewords.png" alt="Runewords Open" class="icon open-icon">
+            <img src="icons/Runewords_click.png" alt="Runewords Close" class="icon close-icon hidden">
         </button>
         <div class="content">
             <div id="runewords" class="container">
@@ -1394,8 +1394,8 @@ class ClassPagesHTMLGenerator:
         
         return f"""
         <button type="button" class="collapsible uniques-button">
-            <img src="icons/Uniques_click.png" alt="Uniques Open" class="icon open-icon">
-            <img src="icons/Uniques.png" alt="Uniques Close" class="icon close-icon hidden">
+            <img src="icons/Uniques.png" alt="Uniques Open" class="icon open-icon">
+            <img src="icons/Uniques_click.png" alt="Uniques Close" class="icon close-icon hidden">
         </button>    
         <div class="content">   
             <div id="uniques" class="container">
@@ -1435,8 +1435,8 @@ class ClassPagesHTMLGenerator:
         
         return f"""
         <button type="button" class="collapsible sets-button">
-            <img src="icons/Sets_click.png" alt="Sets Open" class="icon open-icon">
-            <img src="icons/Sets.png" alt="Sets Close" class="icon close-icon hidden">
+            <img src="icons/Sets.png" alt="Sets Open" class="icon open-icon">
+            <img src="icons/Sets_click.png" alt="Sets Close" class="icon close-icon hidden">
         </button>  
         <div class="content">  
             <div id="sets" class="container">
@@ -1483,8 +1483,8 @@ class ClassPagesHTMLGenerator:
         </h2>
         <h3>This is base synthesized items</h3>
         <button type="button" class="collapsible sets-button">
-            <img src="icons/Special_click.png" alt="Synth Open" class="icon open-icon">
-            <img src="icons/Special.png" alt="Synth Close" class="icon close-icon hidden">
+            <img src="icons/Special.png" alt="Synth Open" class="icon open-icon">
+            <img src="icons/Special_click.png" alt="Synth Close" class="icon close-icon hidden">
         </button>  
         <div class="content">  
             <div id="special">
@@ -1500,8 +1500,8 @@ class ClassPagesHTMLGenerator:
         </h2>
         <h3>This shows where propertied an item are showing up in other items. If you wanted to see where the slow from Kelpie or the Ball light from Ondal's had popped up, this is where to look </h3>
         <button type="button" class="collapsible sets-button">
-            <img src="icons/Special_click.png" alt="Synth Open" class="icon open-icon">
-            <img src="icons/Special.png" alt="Synth Close" class="icon close-icon hidden">
+            <img src="icons/Special.png" alt="Synth Open" class="icon open-icon">
+            <img src="icons/Special_click.png" alt="Synth Close" class="icon close-icon hidden">
         </button>  
         <div class="content">  
             <div id="special">
@@ -1524,8 +1524,8 @@ class ClassPagesHTMLGenerator:
         <h3>{equipment_analysis['craft_user_count']} Characters with crafted items equipped</h3>
 
         <button type="button" class="collapsible sets-button">
-            <img src="icons/Special_click.png" alt="Synth Open" class="icon open-icon">
-            <img src="icons/Special.png" alt="Synth Close" class="icon close-icon hidden">
+            <img src="icons/Special.png" alt="Synth Open" class="icon open-icon">
+            <img src="icons/Special_click.png" alt="Synth Close" class="icon close-icon hidden">
         </button>  
         <div class="content">  
             <div id="special">
@@ -1548,8 +1548,8 @@ class ClassPagesHTMLGenerator:
         <h3>{equipment_analysis['magic_user_count']} Characters with Magic items equipped</h3>
 
         <button type="button" class="collapsible sets-button">
-            <img src="icons/Special_click.png" alt="Synth Open" class="icon open-icon">
-            <img src="icons/Special.png" alt="Synth Close" class="icon close-icon hidden">
+            <img src="icons/Special.png" alt="Synth Open" class="icon open-icon">
+            <img src="icons/Special_click.png" alt="Synth Close" class="icon close-icon hidden">
         </button>  
         <div class="content">  
             <div id="special">
@@ -1572,8 +1572,8 @@ class ClassPagesHTMLGenerator:
         <h3>{equipment_analysis['rare_user_count']} Characters with rare items equipped</h3>
 
         <button type="button" class="collapsible sets-button">
-            <img src="icons/Special_click.png" alt="Synth Open" class="icon open-icon">
-            <img src="icons/Special.png" alt="Synth Close" class="icon close-icon hidden">
+            <img src="icons/Special.png" alt="Synth Open" class="icon open-icon">
+            <img src="icons/Special_click.png" alt="Synth Close" class="icon close-icon hidden">
         </button>  
         <div class="content">  
             <div id="special">
@@ -1595,8 +1595,8 @@ class ClassPagesHTMLGenerator:
         <h3>What are people puting in sockets</h3>
 
         <button type="button" class="collapsible sets-button">
-            <img src="icons/Special_click.png" alt="Synth Open" class="icon open-icon">
-            <img src="icons/Special.png" alt="Synth Close" class="icon close-icon hidden">
+            <img src="icons/Special.png" alt="Synth Open" class="icon open-icon">
+            <img src="icons/Special_click.png" alt="Synth Close" class="icon close-icon hidden">
         </button>  
         <div class="content">  
             <h2>Socketed Runes Count</h2>
@@ -1644,8 +1644,8 @@ class ClassPagesHTMLGenerator:
         </h3>
 
         <button type="button" class="collapsible">
-            <img src="icons/Merc_click.png" alt="Merc Details Open" class="icon open-icon">
-            <img src="icons/Merc.png" alt="Merc Details Close" class="icon close-icon hidden">
+            <img src="icons/Merc.png" alt="Merc Details Open" class="icon open-icon">
+            <img src="icons/Merc_click.png" alt="Merc Details Close" class="icon close-icon hidden">
         </button>
         <div class="content">
             <div id="mercequips">
@@ -1705,8 +1705,8 @@ class ClassPagesHTMLGenerator:
         <h3 id="fun-facts">{section_title} <a href="#fun-facts" class="anchor-link"><img src="icons/anchor.png" alt="🔗" class="anchor-icon"></a></h3>
         <h3>{death_text}</h3>
         <button type="button" class="collapsible sets-button">
-            <img src="icons/Special_click.png" alt="Undead Open" class="icon open-icon">
-            <img src="icons/Special.png" alt="Undead Close" class="icon close-icon hidden">
+            <img src="icons/Special.png" alt="Undead Open" class="icon open-icon">
+            <img src="icons/Special_click.png" alt="Undead Close" class="icon close-icon hidden">
         </button>
         <div class="content">  
             <div id="special">{alive_list_html}</div>
@@ -2118,8 +2118,8 @@ class ClassPagesHTMLGenerator:
             </h3>
             <p>These skills have been maxed (20 points) by one or more characters.</p>
             <button type="button" class="collapsible sets-button">
-                <img src="icons/Special_click.png" alt="Undead Open" class="icon open-icon">
-                <img src="icons/Special.png" alt="Undead Close" class="icon close-icon hidden">
+                <img src="icons/Special.png" alt="Undead Open" class="icon open-icon">
+                <img src="icons/Special_click.png" alt="Undead Close" class="icon close-icon hidden">
             </button>
             <div class="content">  
                 <div id="special">{section_html}</div>
